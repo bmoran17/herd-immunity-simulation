59187a19264d88d3456fb80b4471aad59690a3c0
bmoran17 <brendamoran1999@gmail.com>
2023-10-01 00:40:41 -0700

    adding help tips to form

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import Virus from "./utils/virus";
+import Timestep from "./components/Timestep";
+import Population from "./components/Population";
+import "./app.css";
 
 function App() {
-  const [popSize, setPopSize] = useState();
-  const [vaccPerc, setVaccPerc] = useState();
-  const [initialInfected, setInitialInfected] = useState();
-  const [interactions, setInteractions] = useState();
+  const [popSize, setPopSize] = useState(0);
+  const [vaccPerc, setVaccPerc] = useState(0);
+  const [initialInfected, setInitialInfected] = useState(0);
+  const [interactions, setInteractions] = useState(0);
   const [virusName, setVirusName] = useState("");
-  const [virusReproRate, setVirusReproRate] = useState();
-  const [virusMortaRate, setVirusMortaRate] = useState();
+  const [virusReproRate, setVirusReproRate] = useState(0);
+  const [virusMortaRate, setVirusMortaRate] = useState(0);
   const [beginSimulation, setBeginSimulation] = useState(false);
 
+  const renderSimulation = () => {
+    setBeginSimulation(!beginSimulation);
+  }
+
+  const data = [10, 2, 5, 1, 3, 12 ];
+
   const startSimulation = () => {
-    const virus = new Virus(virusName, virusReproRate, virusReproRate);
+    const virus = new Virus(virusName, virusReproRate, virusMortaRate);
+    console.log(virus)
     // const simulation = new Simulation(popSize, vaccPerc, initialInfected, interactions);
   }
 
   useEffect(() => {
-    startSimulation()
-  }, [])
-
-  
+    if (popSize > 0) {
+      startSimulation()
+    }
+  }, [beginSimulation]);
 
   return (
     <>
     <h1>Herd Immunity Simulation</h1>
     <Form 
+      renderSimulation={renderSimulation}
       setPopSize={setPopSize} 
       setVaccPerc={setVaccPerc} 
       setInitialInfected={setInitialInfected}
@@ -35,6 +46,8 @@ function App() {
       setVirusReproRate={setVirusReproRate}
       setVirusMortaRate={setVirusMortaRate}
     />
+    <Population />
+    <Timestep data={data} />
     </>
   );
 }
---
+++ src/components/Population.jsx
@@ -0,0 +1,13 @@
+const Population = () => {
+  return (
+    <div>
+      <h2>Creating Population</h2>
+      <p>People were randomly chosen to get infected and vaccinated.</p>
+      <p>Initial population: </p>
+      <p>Vaccinated: ([vacc rate]% of [inital pop])</p>
+      <p>Infected: </p>
+    </div>
+  )
+}
+
+export default Population;
