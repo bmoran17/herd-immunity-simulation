5e20d0043a0851f2f303653e2ffa884d0a1eab59
bmoran17 <brendamoran1999@gmail.com>
2023-11-30 23:56:48 -0800

    sample virus and simulation data for testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Virus from './utils/virus';
 
 function App() {
   const virus1 = new Virus ("Ebola", .21, .27 )
-  const simulation = new Simulation (20000, .10, 100, virus1, 10)
+  const simulation = new Simulation (3000, .30, 3, virus1, 12)
   simulation.run()
   return (
     <div className="App">
